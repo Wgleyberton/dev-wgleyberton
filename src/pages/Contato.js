e6f5de58ecacadcd47864b38cce087867d7b3bd1
Wgleyberton Alencar <wgleybertonds@gmail.com>
2023-06-14 00:55:55 -0300

    corrigindo projeto

--- src/pages/Contato.js
+++ src/pages/Contato.js
@@ -2,6 +2,7 @@ import React from "react"
 import { useState } from "react"
 import "./Contato.css"
 import emailjs from "@emailjs/browser"
+import { Link } from "react-router-dom"
 
 
 const Contato = () => {
